b950782e5b2eb1a0d710d2cf53390df48ae4cecf
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-28 20:35:58 +0000

    typescriptlessons

--- tslesson1.js
+++ tslesson1.js
@@ -6,3 +6,12 @@ function addNumbers( a, b ) {
 
 console.log(addNumbers("5", "6"))
 
+//Types in JavaScript
+
+function response( a, b ) {
+    return a + b
+}
+//only put in numbers!
+console.log(response("5", "6"))
+
+
