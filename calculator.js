d2a3663434353d61898158c7e273a2195e514568
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-28 21:17:46 +0000

    typescriptlessons

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// Using typeof
+
+const button = document.querySelector('.button')
+const firstInput = document.querySelector('#first-input')
+const secondInput = document.querySelector('#second-input')
+const screen = document.querySelector('.screen')
+
+function addNumbers(a,b) {
+        console.log(a+b)
+    screen.innerHTML = a + b
+
+}
+
+button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+
