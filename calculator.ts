07f9236021d4d37e3e7b7e007b7e55c05c906e61
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-29 09:45:26 +0000

    updates

--- calculator.ts
+++ calculator.ts
@@ -8,8 +8,9 @@ const firstInput = document.querySelector('#first-input') as HTMLInputElement
 const secondInput = document.querySelector('#second-input') as HTMLInputElement
 export const screen = document.querySelector('.screen')
  
-function addNumbers(a,b) {
-     screen.innerHTML = a + b     
+function addNumbers(a: number,b: number) {
+     screen.innerHTML = (a + b).toString()
 }
  
-button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+button.addEventListener('click', () => 
+    addNumbers(parseInt(firstInput.value), parseInt(secondInput.value))
