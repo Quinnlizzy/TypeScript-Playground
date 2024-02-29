05c759c41a16994cde2026fab3191a944bfc49e3
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-29 09:39:30 +0000

    updates

--- calculator.ts
+++ calculator.ts
@@ -1,15 +1,15 @@
-// Using typeof
+// Benefits of TypeScript
+// challenges:
+// - give the screen's innerHTML a string
+// - change the two input values to numbers
 
 const button = document.querySelector('.button')
-const firstInput = document.querySelector('#first-input')
-const secondInput = document.querySelector('#second-input')
-const screen = document.querySelector('.screen')
-
+const firstInput = document.querySelector('#first-input') as HTMLInputElement
+const secondInput = document.querySelector('#second-input') as HTMLInputElement
+export const screen = document.querySelector('.screen')
+ 
 function addNumbers(a,b) {
-        console.log(a+b)
-    screen.innerHTML = a + b
-
+     screen.innerHTML = a + b     
 }
-
+ 
 button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
-
